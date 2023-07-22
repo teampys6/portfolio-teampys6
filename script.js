d72f65cd03f54b992e23f8a8a1ebd6744a1d3846
Arthur Luiz <flashgames502@gmail.com>
2023-07-22 03:47:25 -0300

    update script.js - 0.1

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const navEl = document.querySelector('.nav');
 const hamburgerEl = document.querySelector('.hamburger');
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('#home', { 
+    duration: 1000,
+    scale: 0.1,
+})
+
+sr.reveal('#about', { 
+  duration: 1000,
+  scale: 0.1,
+})
+
+sr.reveal('#contacts', { 
+  duration: 1000,
+  scale: 0.1,
+})
+
+sr.reveal('#projects', { 
+  duration: 1000,
+  scale: 0.1,
+})
+
+sr.reveal('#skills', { 
+  duration: 1000,
+  distance: '40px',
+})
 
 hamburgerEl.addEventListener('click', () => {
   navEl.classList.toggle('nav--open');
